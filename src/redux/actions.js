dde94d2452688093c0900a53ed2025ab55b0d16e
Urpiccha <ur.picha.hartman@gmail.com>
2019-12-07 20:39:45 -0600

    Here it is

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,15 +1,41 @@
-const url = "https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json"
+const url =
+	"https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json";
 
-export const addCar = (car) => {
-    return {
-        type: 'ADD_CAR',
-        value: car
-    }
-}
+// export const ADD_CAR = "ADD_CAR";
+// export const REMOVE_CAR = "REMOVE_CAR";
+// export const DELETE_MAKE = "DELETE_MAKE";
 
-export const removeCar = (index) => {
-    return {
-        type: 'REMOVE_CAR',
-        value: index
-    }
-}
+export const addCar = car => {
+	return {
+		type: "ADD_CAR",
+		value: car
+	};
+};
+
+export const removeCar = index => {
+	return {
+		type: "REMOVE_CAR",
+		value: index
+	};
+};
+
+export const deleteMake = index => {
+	return {
+		type: "DELETE_MAKE",
+		value: index
+	};
+};
+
+export const fetchMakes = () => {
+	return dispatch => {
+		fetch(url)
+			.then(res => res.json())
+			.then(response => {
+				const action = {
+					type: "FETCH_MAKES",
+					value: response.Results
+				};
+				dispatch(action);
+			});
+	};
+};
